a71277ef75a6b81cf996fd6cf5482a16b2e88c61
Harald Svendberg <harald@codemover.com>
2020-11-18 19:25:23 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## What?
+
+Source for the codemover.com website.
---
+++ pages/index.tsx
@@ -0,0 +1,24 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Codemover</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Codemover
+        </h1>
+        <p className={styles.description}>
+          Kubernetes, JavaScript, TypeScript, .NET, Python, etc...
+        </p>
+        <p>
+          <a href="mailto: harald@codemover.com">harald@codemover.com</a>
+        </p>
+      </main>
+    </div>
+  )
+}
