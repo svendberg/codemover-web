aea29ffdbaeb4c5fb9c8461810818c619ac25b5e
Harald Svendberg <--replace-all>
2021-05-16 18:27:52 +0200

    updated dependencies

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,15 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
         <title>Codemover</title>
-        <link rel="icon" href="/favicon.ico" />
+        {/* <link rel="icon" href="/favicon.ico" /> */}
       </Head>
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Codemover
-        </h1>
+        <h1 className={styles.title}>Codemover</h1>
         <p className={styles.description}>
           Kubernetes, JavaScript, TypeScript, .NET, Python, etc...
         </p>
@@ -20,5 +18,5 @@ export default function Home() {
         </p>
       </main>
     </div>
-  )
+  );
 }
